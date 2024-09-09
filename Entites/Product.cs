429a20a7a8f68a3337a9c090d9a7fc18faf3c163
Juliano Martins de Souza <julianopoamartins@gmail.com>
2024-09-08 21:31:24 -0300

    Adicionar arquivos de projeto.

---
+++ Entites/Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analise_dados_Produtos.Entites {
+    internal class Product {
+
+
+        // Atributos de classe
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+
+        // Construtor
+        public Product(string name, double price) {
+            Name = name;
+            Price = price;
+        }
+
+
+        // Métodos
+        public override string ToString() {
+            string valorFormatado = string.Format(CultureInfo.GetCultureInfo("pt-BR"), "{0:C}", Price);
+            return $"Produto: {Name} {valorFormatado}";
+        }
+
+        public override bool Equals(object obj) {
+            return obj is Product product &&
+                   Name == product.Name;
+        }
+
+        public override int GetHashCode() {
+            return 539060726 + EqualityComparer<string>.Default.GetHashCode(Name);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using Analise_dados_Produtos.Entites;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Analise_dados_Produtos {
+    internal class Program {
+        static void Main(string[] args) {
+
+            string path = @"D:\WorkSpace\Analise_dados_Produtos/Arquivo.txt";
+
+            List<Product> prod = new List<Product>();
+
+
+            using (StreamReader sr = File.OpenText(path)) {
+
+                while (!sr.EndOfStream) {
+                    string[] vetor = sr.ReadLine().Split(',');
+                    string name = vetor[0];
+                    double price = double.Parse(vetor[1], CultureInfo.InvariantCulture);
+
+                    prod.Add(new Product(name, price));
+
+                }
+            }
+
+            double media = prod.Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+            string valorFormatado = string.Format(CultureInfo.GetCultureInfo("pt-BR"), "{0:C}", media);
+
+            Console.WriteLine("Média de preço dos produtos" + valorFormatado);
+
+            var abaixoMedia = prod.Where(p => p.Price < media).OrderByDescending(p => p.Name).Select(p => p.Name);
+
+            Console.WriteLine("\nLista de produtos abaixo da média em ordem descrecente");
+
+
+            foreach(var p in abaixoMedia) {
+                
+                Console.WriteLine(p.ToString().ToUpper());
+            }
+
+        }
+    }
+}
